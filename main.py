4b6a530253188884f91817b30ae90e0c1b891d5b
yasminebosle <fareedahusman@ryanacademy.shc.ng>
2024-10-18 14:46:21 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+a = 10
+f = 12
+e = 12
+
+print(a != f)
+print(f != e)
+
+a = "amanna"
+f = "fareedah"
+
+if a != f :
+    print(a, 'and', f, 'are diffrent.')
+
+a = 4
+f = 5
+
+if (a == 1) != (f == 5):
+    print('Hello')
+
+
+
+
+
+
+    
